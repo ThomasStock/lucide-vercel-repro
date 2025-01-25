551fc8ba107e511c3735415ba8e2efc4c230dd5d
ThomasStock <thomasstock1985@gmail.com>
2025-01-25 14:09:53 +0100

    icon only

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,16 +5,9 @@ import vercel from "@astrojs/vercel";
 
 import react from "@astrojs/react";
 
-import tailwind from "@astrojs/tailwind";
-
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: vercel(),
-  integrations: [
-    react(),
-    tailwind({
-      applyBaseStyles: false,
-    }),
-  ],
+  integrations: [react()],
 });
